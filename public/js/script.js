70b126c5e344fb25c3043c43461fd9f77404c13e
AlineStrass <flores.aline.89@gmail.com>
2022-11-09 21:08:43 -0300

    projeto wecode

---
+++ public/js/script.js
@@ -0,0 +1,12 @@
+document.getElementById('openMenu').onclick = function(){
+    console.log('teste abre menu')
+    document.querySelector('.lateral-menu').style.display = 'block';
+}
+
+
+document.getElementById('btn-close-menu').onclick = function(){
+    console.log("testessss")
+    document.querySelector('.lateral-menu').style.display = 'none';
+}
+
+
